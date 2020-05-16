d8e24efd506af5bbbf1ab60ae08a7e3440202a4f
Paul Beckwith <303dog@gmail.com>
2020-05-16 17:33:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,10 @@ class Song
     @@all
   end
   
+    def add_song(song)
+    @songs << song
+  end
+  
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
