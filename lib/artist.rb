4c6d4d65f019b8d3b7282abb7774ca474205d7fa
Paul Beckwith <303dog@gmail.com>
2020-05-16 17:33:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,9 +27,9 @@ class Artist
     self.find(name) || self.create(name)
   end
 
-  def add_song(song)
-    @songs << song
-  end
+ # def add_song(song)
+  #  @songs << song
+#  end
 
   def print_songs
     songs.each { |song| puts song.name }
