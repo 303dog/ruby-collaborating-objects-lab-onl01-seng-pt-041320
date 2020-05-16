19f47266e86db79d943fffa334ca367f5c137db2
Paul Beckwith <303dog@gmail.com>
2020-05-16 17:11:57 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end+with?(".MP3")}
+  end 
+  
+  def import 
+    files.each {|song| Song.new_by__filename(song)}
+  end 
+end
